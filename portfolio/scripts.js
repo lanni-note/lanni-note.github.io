87aec66a75fb62e3f28e58a1241bc5acb6aba92a
liuutin9 <liuutin9@gmail.com>
2026-03-17 12:08:29 -0500

    update

--- portfolio/scripts.js
+++ portfolio/scripts.js
@@ -2,31 +2,31 @@
 const portfolioData = [
     {
         symbol: 'BTC', name: 'Bitcoin',
-        historicalPrices:   [66959.03, 71333.08],
-        historicalAvgCosts: [67941.44, 68037.23],
-        historicalAmounts:  [0.0004167, 0.0008215],
-        historicalInvested: [28, 56]          
+        historicalPrices:   [66959.03, 71333.08, 74205.38],
+        historicalAvgCosts: [67941.44, 68037.23, 69188.37],
+        historicalAmounts:  [0.0004167, 0.0008215, 0.001213],
+        historicalInvested: [28, 56, 84]
     },
     {
         symbol: 'ETH', name: 'Ethereum',
-        historicalPrices:   [1993.78, 2081.02],
-        historicalAvgCosts: [1983.95, 2001.64],
-        historicalAmounts:  [0.003518, 0.006986],
-        historicalInvested: [7, 14]           
+        historicalPrices:   [1993.78, 2081.02, 2328.51],
+        historicalAvgCosts: [1983.95, 2001.64, 2042.68],
+        historicalAmounts:  [0.003518, 0.006986, 0.01026],
+        historicalInvested: [7, 14, 21]
     },
     {
         symbol: 'SOL', name: 'Solana',
-        historicalPrices:   [85.85, 88.52],
-        historicalAvgCosts: [81.35, 83.82],
-        historicalAmounts:  [0.02998, 0.06996],
-        historicalInvested: [3, 6]            
+        historicalPrices:   [85.85, 88.52, 94.62],
+        historicalAvgCosts: [81.35, 83.82, 84.40],
+        historicalAmounts:  [0.02998, 0.06996, 0.09995],
+        historicalInvested: [3, 6, 9]
     },
     {
         symbol: 'SUI', name: 'Sui',
-        historicalPrices:   [0.9127, 0.996],
-        historicalAvgCosts: [0.87, 0.8864],
-        historicalAmounts:  [4.4977, 8.8955],
-        historicalInvested: [4, 8]            
+        historicalPrices:   [0.9127, 0.996, 1.0275],
+        historicalAvgCosts: [0.87, 0.8864, 0.9063],
+        historicalAmounts:  [4.4977, 8.8955, 13.0934],
+        historicalInvested: [4, 8, 12]
     }
 ];
 
